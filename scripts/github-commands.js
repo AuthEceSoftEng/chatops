484eace6344d5f23429cfa588691a8fb5f567d20
andreash92 <andreas_h92@outlook.com>
2017-05-13 13:51:46 +0300

    created github-commands.js
    authenticate a github account using personal access token and fetching some info e.g. user followers

---
+++ scripts/github-commands.js
@@ -0,0 +1,63 @@
+module.exports = function(robot) {
+
+
+	/* set Github Account */
+	var GitHubApi = require("github");
+
+	var github = new GitHubApi({
+	    /* optional */
+	    // debug: true,
+	    // protocol: "https",
+	    // host: "api.github.com", // should be api.github.com for GitHub
+	    // //thPrefix: "/api/v3", // for some GHEs; none for GitHub
+	    // headers: {
+	    //     "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+	    // },
+	    // Promise: require('bluebird'),
+	    // followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+	    // timeout: 5000
+	});
+
+
+    /* oauth autentication using github personal token */
+	github.authenticate({
+	    "type": "oauth",
+	    "token": process.env.HUBOT_GITHUB_TOKEN
+	});
+
+    /* basic autentication using github's username & password */
+	// github.authenticate({
+	//     type: "basic",
+	//     username: '',
+	//     password: ''
+	// });
+
+	robot.respond(/gh followers (.*)/i, function(res_r) {
+		var username = res_r.match[1];
+
+		res_r.reply(username)
+
+		github.users.getFollowersForUser({ 
+			"username": username}, 
+			function(err,res){
+				var jsonsize = Object.keys(res.data).length;
+				var menu = require('./menu.json');
+				menu.attachments[0].actions[0]['options'].push({"text":"","value":""});
+
+				var login;
+				for (i = 0; i < jsonsize; i++) {
+					login = res.data[i].login;
+					menu.attachments[0].actions[0]['options'].push({"text":login,"value":login})
+      			}
+      			menu.attachments[0].text = 'Followers';
+      			menu.attachments[0].fallback = '';
+      			menu.attachments[0].callback_id = 'followers_cb_id';
+      			menu.attachments[0].actions[0].name=' ';
+				menu.attachments[0].actions[0].text=' ';
+
+				res_r.reply(menu);
+
+			});	
+	})
+
+}
